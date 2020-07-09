5e764a0f8f2f5757ed6397710351ab94b16fe477
Matthew Luc Sterling <mlucsterling@gmail.com>
2020-07-09 12:58:25 -0700

    finalizing HTML, JS, and CSS

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function makeRequest() {
     fetch(requestAPI)
         .then(response => response.json())
         .then(responseJson => createList(responseJson))
-        .catch(error => alert(`either that user does not exist, or my code still sucks`));
+        .catch(error => {
+            console.log(error)
+            alert(`either that user does not exist, or my code still sucks`)
+         });
 };
 
 /*handles form submission*/
@@ -24,17 +27,16 @@ function submitForm() {
 function createList(responseJson) {
     console.log(responseJson);
     console.log(`createList activated`);
-    $(`watch-this`).removeClass('hidden');
-    $(`.results-container`).empty();
-        for (let i=0; i < responseJson.message.length; i++) {
-            /*create variables for each item*/
-            let repoURL = responseJson.message[i].html_url
-            let repoName = responseJson.message[i].name
-
-            $(`.results-container`).append(`<li>
+    /*$('.hide').removeClass('.hide');*/
+    $(`.results-list`).empty();
+        for (let i=0; i < responseJson.length; i++) {
+            console.log('creating list item');
+            let repoURL = responseJson[i].html_url
+            let repoName = responseJson[i].name
+            $(`.results-list`).append(`<li>
             <h4>${repoName}</h4>
             <a href="${repoURL}">Click here for repository</a>
-            </li>`)
+            </li>`);
     };
 };
 
